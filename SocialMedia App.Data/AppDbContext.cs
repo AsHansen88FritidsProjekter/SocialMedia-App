372cca37a777f3b6af866c8d8f8161bab33d0dec
AsHansen88 <ande970s@stud.kea.dk>
2024-11-13 12:22:49 +0100

    add db user table

--- SocialMedia App.Data/AppDbContext.cs
+++ SocialMedia App.Data/AppDbContext.cs
@@ -7,11 +7,12 @@ namespace SocialMedia_App.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
         { 
-        
         }
         
         public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
 
-}
+        
     }
+}
 
---
+++ SocialMedia App.Data/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia_App.Data.Models
+{
+     public class User
+    {
+
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+        
+        public string ProfilePictureUrl {  get; set; }
+
+    }
+}
