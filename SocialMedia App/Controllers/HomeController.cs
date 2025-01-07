ff36a99cc914006ddf56e141c432aec0a9de6ea4
AsHansen88 <ande970s@stud.kea.dk>
2025-01-07 14:21:22 +0100

    Igang igen

--- SocialMedia App/Controllers/HomeController.cs
+++ SocialMedia App/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialMedia_App.Data;
 using System.Diagnostics;
 
 namespace SocialMedia_App.Controllers
@@ -6,15 +8,21 @@ namespace SocialMedia_App.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task <IActionResult> Index()
         {
+            var allPosts = await _context.Posts
+                .Include (n => n.User)
+                .ToListAsync();
             return View();
+
         }
 
     }
